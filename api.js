695f052408a44cb1e74361532518c22bbae3ceb5
DominikTenerife <121733718+DominikTenerife@users.noreply.github.com>
2023-09-05 16:07:29 +0100

    connecting firebase

--- api.js
+++ api.js
@@ -1,4 +1,37 @@
-export async function getVans(id) {
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTJqy4CL7hPbPPF7CuWQ9QE41BX5wZWAQ",
+  authDomain: "vanlife-b82e4.firebaseapp.com",
+  projectId: "vanlife-b82e4",
+  storageBucket: "vanlife-b82e4.appspot.com",
+  messagingSenderId: "939540913954",
+  appId: "1:939540913954:web:608a685ccdc3bb5abb4f7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans")
+
+
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+
+/**export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
     if (!res.ok) {
@@ -11,6 +44,7 @@ export async function getVans(id) {
     const data = await res.json()
     return data.vans
 }
+*/
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
